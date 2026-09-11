69dd723c4e91cb265513832c02832580460b5926
muxi01 <mrsha1195@163.com>
2026-09-11 10:13:27 +0000

    update for sound card

--- modules_install.sh
+++ modules_install.sh
@@ -4,7 +4,7 @@ if [ "$INSTALL_MOD_PATH" = "" ] ; then
     echo "INSTALL_MOD_PATH is not set"
 else
     echo "INSTALL_MOD_PATH is set to $INSTALL_MOD_PATH"
-    make modules 
+    make modules  -j4
     make modules_install 
     # depmod -a -s $INSTALL_MOD_PATH
 fi 
